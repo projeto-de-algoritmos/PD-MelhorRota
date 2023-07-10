d77b47113b05524f289967c23b57bdb8c8877629
Júlia Farias Sousa <60350851+julisous@users.noreply.github.com>
2023-07-09 22:24:48 -0300

    Implementando função calcular_custo
    Co-authored-by: Yuri Alves<Yurivalerio12@gmail.com>

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import numpy as np
 
-def calcular_custo(origem, destino):
+def calcular_custo(origem, destino, peso):
     # Função para calcular o custo entre um ponto de origem e um destino
-    # Pode ser baseado em fatores como distância, tempo, prioridade, etc.
-    # Retorna o custo total
+    # Neste exemplo, o custo será baseado na distância multiplicada pelo peso do pacote
+    distancia = abs(destino - origem)
+    return distancia * peso
 
-def encontrar_rota_otimizada(origens, destinos, capacidade_veiculo):
+def encontrar_rota_otimizada(origens, destinos, pesos, capacidade_veiculo):
     n = len(origens)
     dp = np.zeros((n, capacidade_veiculo+1))
 
     for i in range(n):
         for j in range(capacidade_veiculo+1):
-            if j >= destinos[i]:
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-destinos[i]] + calcular_custo(origens[i], destinos[i]))
+            if j >= pesos[i]:
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-pesos[i]] + calcular_custo(origens[i], destinos[i], pesos[i]))
             else:
                 dp[i][j] = dp[i-1][j]
     
     # Recupera a rota ótima a partir da tabela dp
     rota_otimizada = []
     i, j = n-1, capacidade_veiculo
-    while i > 0 and j > 0:
-        if dp[i][j] != dp[i-1][j]:
+    while i >= 0 and j >= 0:
+        if i == 0 and dp[i][j] != 0:
             rota_otimizada.append((origens[i], destinos[i]))
-            j -= destinos[i]
+            break
+        elif dp[i][j] != dp[i-1][j]:
+            rota_otimizada.append((origens[i], destinos[i]))
+            j -= pesos[i]
         i -= 1
 
     return rota_otimizada[::-1]
@@ -30,9 +34,10 @@ def encontrar_rota_otimizada(origens, destinos, capacidade_veiculo):
 # Exemplo de uso do sistema de recomendação de rotas otimizadas
 origens = [1, 2, 3, 4]  # Pontos de origem
 destinos = [2, 4, 1, 3]  # Pontos de destino
+pesos = [2, 1, 3, 2]  # Pesos dos pacotes
 capacidade_veiculo = 5
 
-rota = encontrar_rota_otimizada(origens, destinos, capacidade_veiculo)
+rota = encontrar_rota_otimizada(origens, destinos, pesos, capacidade_veiculo)
 print("Rota otimizada:")
 for origem, destino in rota:
     print(f"Origem: {origem}, Destino: {destino}")
